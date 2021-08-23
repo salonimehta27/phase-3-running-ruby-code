779b2c7682590517979a29e81ce6df05d3f5506b
Saloni <salonimehta2127@gmail.com>
2021-08-23 15:39:10 -0400

    commit tree 575f11a21940701bdaae9977627d58116aedf725

---
+++ app.rb
@@ -0,0 +1,13 @@
+puts "Hello World!" #preferred syntax
+print "Pass this test, please. "
+#app.rb this is how to write comment in ruby
+#puts("Hello World") writing paranthesis to methods is optional
+# puts "Hello world!"
+# puts "Hello world!"
+# print prints the text all in the same line 
+# print "Hello World!"
+# print "Hello World!"
+# print "Hello World!" 
+p [1,2,3]
+# hashes are ruby equivalent of javaScripts objects
+#pp [{id:1, hello:"World"},{id:2, hello:"Ruby"},{id:3, hello:"Moon"},{id:4, hello:"Learner"}]
